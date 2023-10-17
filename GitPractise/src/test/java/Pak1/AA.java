dfbef5a96736f16622906f16172de2f32f21d817
qaShoaib <qashoaibakhtar@gmail.com>
2023-10-17 13:05:33 +0530

    first commit
    m method added

---
+++ GitPractise/src/test/java/Pak1/AA.java
@@ -0,0 +1,11 @@
+package Pak1;
+
+public class AA {
+	public static void main(String[] args) {
+		System.out.println("Welcome!!!");
+	}
+	public void M() {
+		System.out.println("From Method M");
+	}
+
+}
