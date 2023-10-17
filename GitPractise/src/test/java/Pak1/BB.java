d69d0616330531897a29e405f3d0c40d76cdc72c
qaShoaib <qashoaibakhtar@gmail.com>
2023-10-17 13:33:00 +0530

    Created New class BB
    main and java method is added

---
+++ GitPractise/src/test/java/Pak1/BB.java
@@ -0,0 +1,11 @@
+package Pak1;
+
+public class BB {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	public void Java() {
+		System.out.println("Hello Java");
+	}
+
+}
